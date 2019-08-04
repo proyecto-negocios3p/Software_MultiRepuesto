baa47cf62ba7aa35ef051b68b35f2b84e4d09a56
Diego Buezo <diegobuezo2016@gmail.com>
2019-08-03 20:25:09 -0600

    Se agregor las ventanas que se van a utilizar

---
+++ MultiRepuestos/MultiRepuestos/View/Planilla/Buscar_Planilla.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MultiRepuestos.View.Planilla
+{
+    /// <summary>
+    /// Lógica de interacción para Buscar_Planilla.xaml
+    /// </summary>
+    public partial class Buscar_Planilla : Window
+    {
+        public Buscar_Planilla()
+        {
+            InitializeComponent();
+        }
+    }
+}
