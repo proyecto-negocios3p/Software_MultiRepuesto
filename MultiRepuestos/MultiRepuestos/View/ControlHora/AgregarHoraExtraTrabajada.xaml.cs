556fbc0dbfdea0a1c89c0cfd65068af576ce9412
jackeline varela vasquez <jackelinevarela2@gmail.com>
2019-08-05 00:16:31 -0600

    Código listo para Agregar horas extras del empleado

--- MultiRepuestos/MultiRepuestos/View/ControlHora/AgregarHoraExtraTrabajada.xaml.cs
+++ MultiRepuestos/MultiRepuestos/View/ControlHora/AgregarHoraExtraTrabajada.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +21,65 @@ namespace MultiRepuestos.View.ControlHora
     /// </summary>
     public partial class AgregarHoraExtraTrabajada : Window
     {
+        LinqToSqlDataClassesDataContext dataContext;
+        SqlConnection conexion = new SqlConnection("Data Source = (local)\\SQLEXPRESS; Initial Catalog = PlanillaDePagoMensual; Integrated Security = True");
+        private DataTable tabla;
+
         public AgregarHoraExtraTrabajada()
         {
             InitializeComponent();
+
+            dataContext = new LinqToSqlDataClassesDataContext(conexion);
+
+            MostrarIdentidades();
+            ListarTipoHoras();
+
+        }
+
+        private void ListarTipoHoras()
+        {
+            tabla = new DataTable();
+            try
+            {
+                conexion.Open();
+                string query = "SELECT * FROM Planilla.PorcentajeHoraExtra";
+                SqlDataAdapter adapter = new SqlDataAdapter(query, conexion);
+                using (adapter)
+                {
+                    adapter.Fill(tabla);
+                    cmbHora.DisplayMemberPath = "TipoHora";
+                    cmbHora.SelectedValuePath = "Codigo";
+                    cmbHora.ItemsSource = tabla.DefaultView;
+                    conexion.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void MostrarIdentidades()
+        {
+            tabla = new DataTable();
+            try
+            {
+                conexion.Open();
+                string query = "SELECT * FROM Planilla.Empleado";
+                SqlDataAdapter adapter = new SqlDataAdapter(query, conexion);
+                using (adapter)
+                {
+                    adapter.Fill(tabla);
+                    cmbIdentidad.DisplayMemberPath = "Identidad";
+                    cmbIdentidad.SelectedValuePath = "Identidad";
+                    cmbIdentidad.ItemsSource = tabla.DefaultView;
+                    conexion.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
 
         private void BtnMin_Click(object sender, RoutedEventArgs e)
@@ -33,5 +91,28 @@ namespace MultiRepuestos.View.ControlHora
         {
             this.Close();
         }
+
+        private void BtnAceptar_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Falta condición de campos vacios
+                
+                
+                dataContext.HoraExtra.InsertOnSubmit(new HoraExtra { IdentidadEmpleado = cmbIdentidad.Text, TotalHora = int.Parse(txtHora.Text), Fecha = DateTime.Now, CodigoPorcentajeHoraExtra = cmbHora.SelectedValue.ToString()});
+
+                dataContext.SubmitChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                MessageBox.Show("Se agregaron las horas extra al empleado");
+            }
+        }
     }
+
+    
 }
